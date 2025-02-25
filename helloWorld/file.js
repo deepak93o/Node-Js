be909c4e3e34763ef516a08d4e52d795166741c5
Deepak <deepekbhatt455@gmail.com>
2025-02-25 21:26:39 +0530

    Node js file

---
+++ helloWorld/file.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+///////////Write a File
+
+// fs.writeFileSync('./text.txt', 'Hey There !!');
+
+// fs.writeFile('./text.txt', 'Hey There!!', (err) => {})
+
+// const result = fs.readFileSync('./text.txt', 'utf-8');
+// console.log(result);
+
+//////////Read a file
+
+// fs.readFile("./text.txt","utf-8",(err,result) => {
+//     if(err){
+//         console.log("Error:" + err);        
+//     }
+//     else{
+//         console.log(result);        
+//     }
+// });
+
+///////Append file
+
+// fs.appendFileSync("./text.txt", new Date().getDate().toLocaleString())
+
---
+++ helloWorld/hello.js
@@ -0,0 +1,4 @@
+// console.log("Hello there!!");
+
+const math = require("./math")
+console.log(math);
