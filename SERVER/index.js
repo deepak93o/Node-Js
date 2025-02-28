94f9f23271b5dbac1cf405c653b8c754876137f5
Deepak bhatt <157270970+deepak93o@users.noreply.github.com>
2025-02-28 15:31:00 +0000

    update index.js

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,18 +1,23 @@
 const http = require("http")
 const fs = require("fs")
+const url = require("url");
 
 const myServer = http.createServer( (req, res) => {
+    if (req.url === "/favicon.ico") return res.end();
     const log = `${Date.now()}:${req.url} New Request\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
     fs.appendFile('log.txt', log, (err,data) => {
-        switch(req.url){
+        switch(myUrl.pathname){
             case '/':
                 res.end('Hello from server 1');
             break
             case '/about':
-                res.end('I am Deepak Bhatt');
+                const username = myUrl.query.name
+                res.end(`Hi ${username}!`);
             break
             case '/login':
-                res.end(`${Email}: xyx@gmail.com\n, ${Username}: deepak93o`);
+                res.end(`Email: xyx@gmail.com\nUsername: deepak93o`);
             break
             default: res.end("404 Not Found");
         }
