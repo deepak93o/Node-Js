e356931e8def1de96770cc0d1215ce85f31e5075
Deepak bhatt <157270970+deepak93o@users.noreply.github.com>
2025-03-04 07:45:06 +0000

    Updated main file

--- PROJECT-01/index.js
+++ PROJECT-01/index.js
@@ -3,9 +3,17 @@ const users = require('./MOCK_DATA.json')
 const fs = require("fs");
 const app = express();
 const PORT = 8000;
+
 //Middleware
 app.use(express.urlencoded({ extended: false}));
+app.use((req, res, next) => {
+    fs.appendFile('log.txt', `\n ${Date.now()}: ${req.ip}: ${req.method}: ${req.path}`,
+    (err, data) => {
+        next();
+    });
+});
 
+//Routes
 app.get('/users', (req, res) => {
     const html = `
     <ul>
@@ -15,11 +23,14 @@ app.get('/users', (req, res) => {
     res.send(html);
 });
 
+//Rest API
 app
 .route('/api/users/:id')
 .get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) =>  user.id === id);
+    //http status
+    if(!user) return res.status(404).json({error: "User not found"})
     return res.json(user);
 })
 .patch((req, res) => {
@@ -29,15 +40,20 @@ app
     return res.json({ status: "Pending"})
 });
 
-app.get('/api/users', (req, res) => {    
+app.get('/api/users', (req, res) => { 
+    // console.log(req.headers);
+    // res.setHeader("X-myHeader", "Deepak");
     return res.json(users);
 });
 
 app.post('/api/users', (req, res) => {
     const body = req.body;
+    if(!body || !body.first_name || !body.last_name || !body.email || !body.Country){
+        return res.status(400).json({msg: 'All fields are require'});
+    }
     users.push({...body, id: users.length});
     fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
-        return res.json(`{status: "Success"\n id: ${id}}`);
+        return res.status(201).json({status: "Success"});
     })
 });
 
